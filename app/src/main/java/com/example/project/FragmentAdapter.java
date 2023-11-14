993c58879ff1a7d9982a6c6c6d703091690f8c2b
9gpgv <jtfinu@gmail.com>
2023-11-14 12:46:04 +0530

    small change

---
+++ app/src/main/java/com/example/project/FragmentAdapter.java
@@ -0,0 +1,38 @@
+package com.example.project;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentArrayLis=new ArrayList<>();
+    private ArrayList<String> fragmentTitles=new ArrayList<>();
+    public FragmentAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayLis.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayLis.size();
+    }
+    public void addFragment(Fragment fragment,String title){
+        fragmentArrayLis.add(fragment);
+        fragmentTitles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitles.get(position);
+    }
+}
---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager=findViewById(R.id.ViewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        FragmentAdapter fragmentAdapter=new FragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentAdapter.addFragment(new createActivity(),"Create Activity");
+        fragmentAdapter.addFragment(new ExistingActivity(),"Existing Activity");
+        fragmentAdapter.addFragment(new PendingActivity(),"Pending Activity");
+        fragmentAdapter.addFragment(new InspectorReview(),"Inspector Review");
+        viewPager.setAdapter(fragmentAdapter);
+    }
+
+}
