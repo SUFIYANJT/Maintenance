6949d1332ab5fd8d8afedc576d3e2e1305b90ced
sufiyan <jtfinu@gmal.com>
2023-11-16 14:50:24 +0530

    finacs

--- app/src/main/java/com/example/project/createActivity.java
+++ app/src/main/java/com/example/project/createActivity.java
@@ -7,16 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class createActivity extends Fragment {
-
-
-
+    private EditText activityName,description,name;
+    private Spinner selectMachine,selectComponent,selectSchedule;
+    private Button submit;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        activityName=getActivity().findViewById(R.id.nameactivity);
+        description=getActivity().findViewById(R.id.description);
+        name=getActivity().findViewById(R.id.nameofcreater);
+        selectMachine=getActivity().findViewById(R.id.comboBox);
+        selectComponent=getActivity().findViewById(R.id.comboBox1);
+        selectSchedule=getActivity().findViewById(R.id.comboBox2);
+        submit=getActivity().findViewById(R.id.submit);
+
         return inflater.inflate(R.layout.fragment_create_activity, container, false);
+
     }
 }
