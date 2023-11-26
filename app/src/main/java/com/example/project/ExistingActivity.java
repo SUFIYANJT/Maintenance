5e73356ef979c2b883b09f9f09e7ab8b174e1fd4
karthikeyan-ks <karthikeyanks3673@gmail.com>
2023-11-26 16:06:21 +0530

    change in somewhere

--- app/src/main/java/com/example/project/ExistingActivity.java
+++ app/src/main/java/com/example/project/ExistingActivity.java
@@ -25,6 +25,9 @@ public class ExistingActivity extends Fragment {
         RecyclerView recyclerView=view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         List<DataTable> dataTables=new ArrayList<>();
+        DataTable dataTable1=new DataTable();
+        dataTable1.setData("activity Name","activity Description","History");
+        dataTables.add(dataTable1);
         recyclerView.setAdapter(new CustomRecyclerViewAdapter(dataTables));
         return  view;
     }//dd
